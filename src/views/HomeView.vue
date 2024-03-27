1e24d0239bd0600a48f3d5337df1bd4c366e4119
danwlt <d.woltering1@gmx.de>
2024-03-27 16:41:26 +0100

    fix: change url

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home space-y-6 h-screen flex flex-col items-center justify-center">
     <font-awesome-icon icon="fa-solid fa-binoculars" size="10x"/> 
     <p class="text-3xl font-bold">Welcome to the ISS Auditing Tool</p>
-    <p class="text-xl">Download the tool <a class="text-main-green" href="https://cli.vuejs.org" target="_blank" rel="noopener">here</a>.</p>
+    <p class="text-xl">Download the tool <a class="text-main-green" href="https://github.com/danwlt/iss.security-audit-client" target="_blank" rel="noopener">here</a>.</p>
     <p class="text-lg">Want to learn more about this project? Check out our<br>check out our <RouterLink class="text-main-green" to="/about">about</RouterLink> page</p>
     <p class="text-lg">Have any questions or problems? <br>check out our <RouterLink class="text-main-green" to="/contact">contact</RouterLink> page</p>
   </div>
